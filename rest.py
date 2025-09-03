e760c9be56c6539f82b4d81cd13f97020d042fd2
Angela <angleaxmartinez08@gmail.com>
2025-09-03 14:57:41 -0400

    extra dots, disabled eyetracking, and new rest file

---
+++ rest.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+fill in later for rest rewrite
+"""
+import pandas as pd
+import numpy as np
+from psychopy import visual
+import time
+
+
+def main():
+
+    win = visual.Window([800, 600])
+
+
+if __name__ == "__main__":
+    main() 
