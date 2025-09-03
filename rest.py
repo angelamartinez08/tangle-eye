fb8cb27630d6091440281dcea4bd75f3c849a4a0
creelyhn <creelyhn@upmc.edu>
2025-09-03 15:22:14 -0400

    Added TextStim

--- rest.py
+++ rest.py
@@ -11,6 +11,7 @@ import time
 def main():
 
     win = visual.Window([800, 600])
+    fix = visual.TextStim(win, text='hello')
 #HOMEWORK  
 #create fixed object textsim - fixation cross, draw it, flip it, sleep, 3 sec duration (x3 times)
 #BONUS: look up how to play a sound -- import audio? 
